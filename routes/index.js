f65cbfe55c1794f726c7aa288829862fa3f23617
Alexei Antipov <antipov.alexei@gmail.com>
2014-10-22 17:52:46 +0400

    Add picture layout.

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,20 @@
 var express = require('express');
 var router = express.Router();
 var images = require('../models/paintings');// TODO: fix it.
-var lang = 'en';// TODO: fix it.
+var lang = 'ru';// TODO: fix it.
 var type = 'painting';// TODO: fix it.
 var text = {
     general: require('../lang/general')[lang],
     error: require('../lang/error')[lang]
 };
 
-console.log(text);
-
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('gallery', { title: 'Express', type: type, images: images, lang: lang, text: text});
+router.get('/', function (req, res) {
+    res.render('gallery', { title: 'Express', type: type, images: images, lang: lang, text: text});
+});
+
+router.get('/painting/:image', function (req, res) {
+    res.render('picture', { title: 'Express', type: type, image: req.params.image, info: images[req.params.image], lang: lang, text: text});
 });
 
 module.exports = router;
