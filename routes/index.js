183bd0d694941d49d9a3e22aaf5531c8c3d72d65
Alexei Antipov <antipov.alexei@gmail.com>
2014-10-22 17:30:28 +0400

    Add base layout & gallery layout.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var images = require('../models/paintings');// TODO: fix it.
+var lang = 'en';// TODO: fix it.
+var type = 'painting';// TODO: fix it.
+var text = {
+    general: require('../lang/general')[lang],
+    error: require('../lang/error')[lang]
+};
+
+console.log(text);
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('gallery', { title: 'Express', type: type, images: images, lang: lang, text: text});
 });
 
 module.exports = router;
