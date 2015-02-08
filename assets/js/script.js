464868142e199a443ff1bf8ce93be4aa17e2e313
Alexei Antipov <antipov.alexei@gmail.com>
2015-02-08 03:11:05 +0300

    Setup image modal loading. Setup basic styles.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,52 @@
-Shadowbox.init({
-    // skip the automatic setup again, we do this later manually
-    skipSetup: true
-});
-$(document).ready(function () {
-    if ($('#gallery-wrapper').length) {
-        $('a.lbox').each(function () {
-            Shadowbox.setup(this, {
-                overlayOpacity: 0.8,
-                title: $(this).attr('data-title')
+'use strict';
+(function ($) {
+    $(document).ready(function () {
+        if ($('#gallery-wrapper').length) {
+            $('a.lens').magnificPopup({
+                type: 'image',
+                gallery: {
+                    enabled: true
+                },
+                image: {
+                    // options for image content type
+                    titleSrc: 'data-title'
+                },
+                mainClass: 'mfp-with-zoom', // this class is for CSS animation below
+                zoom: {
+                    enabled: true, // By default it's false, so don't forget to enable it
+                    duration: 300, // duration of the effect, in milliseconds
+                    easing: 'ease-in-out', // CSS transition easing function
+
+                    // The "opener" function should return the element from which popup will be zoomed in
+                    // and to which popup will be scaled down
+                    // By default it looks for an image tag:
+                    opener: function (openerElement) {
+                        // openerElement is the element on which popup was initialized, in this case its <a> tag
+                        // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+                        return openerElement.is('img') ? openerElement : openerElement.find('img');
+                    }
+                }
             });
-        });
 
-        $("img.lazy").show().lazyload({ threshold: 100, effect: "fadeIn" });
-    }
+            // Lazy load
+            $("img.lazy").show().lazyload({threshold: 100, effect: "fadeIn"});
+        }
 
-    // Load image on picture page.
-    if ($('#picture-wrapper').length) {
-        var imgWrapper = $('#picture-wrapper .img-wrapper');
-        var src = imgWrapper.attr('data-src');
-        var alt = imgWrapper.attr('data-alt');
-        imgWrapper.show();
-        $('<img />', {
-            src: src, alt: alt, load: function () {
-                imgWrapper.removeClass('loading');
-                var width = $(this).prop('width');
-                var height = $(this).prop('height');
-                $(this).prop('width', 270).prop('height', 320).css('opacity', 0).appendTo(imgWrapper);
-                $(this).animate({width: width, height: height, opacity: 1}, 600);
-            }
-        });
-    }
-});
+        // Load image on picture page.
+        if ($('#picture-wrapper').length) {
+            var imgWrapper = $('#picture-wrapper .img-wrapper');
+            var src = imgWrapper.attr('data-src');
+            var alt = imgWrapper.attr('data-alt');
+            imgWrapper.show();
+            $('<img />', {
+                src: src, alt: alt, load: function () {
+                    imgWrapper.removeClass('loading');
+                    var width = $(this).prop('width');
+                    var height = $(this).prop('height');
+                    $(this).prop('width', 270).prop('height', 320).css('opacity', 0).appendTo(imgWrapper);
+                    $(this).animate({width: width, height: height, opacity: 1}, 600);
+                }
+            });
+        }
+    });
+})(jQuery);
