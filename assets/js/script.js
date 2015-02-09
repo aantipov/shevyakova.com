6d9602a571a02ac2c9fbb836f5b703eb167b0e48
Alexei Antipov <antipov.alexei@gmail.com>
2015-02-09 22:54:17 +0300

    Turn of image zooming. Small changes to markup & css.

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,7 @@
                 },
                 mainClass: 'mfp-with-zoom', // this class is for CSS animation below
                 zoom: {
-                    enabled: true, // By default it's false, so don't forget to enable it
+                    enabled: false, // By default it's false, so don't forget to enable it
                     duration: 300, // duration of the effect, in milliseconds
                     easing: 'ease-in-out', // CSS transition easing function
 
