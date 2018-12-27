858d14500392c5a309b77b22ea3e8761613b7235
Alexei Antipov <antipov.alexei@gmail.com>
2018-12-27 13:09:08 +0100

    Improve responsiveness

--- lang/error.js
+++ lang/error.js
@@ -1,26 +1,30 @@
 module.exports = {
   en: {
     404: {
-      title: "Alina Shevyakova. Page Not Found",
-      header: "404 Error: Page not Found",
-      descr: '<p>Sorry, but the page you are looking for has not been found. Perhaps it has been deleted.</p>'
+      title: 'Alina Shevyakova. Page Not Found',
+      header: '404 Error: Page not Found',
+      descr:
+        '<p>Sorry, but the page you are looking for has not been found. Perhaps it has been deleted.</p>',
     },
     500: {
-      title: "Alina Shevyakova. Error occured",
-      header: "500 Error: Some error occured",
-      descr: '<p>Sorry, but there is some error occured. We already know about it and working on it.</p>'
-    }
+      title: 'Alina Shevyakova. Error occured',
+      header: '500 Error: Some error occured',
+      descr:
+        '<p>Sorry, but there is some error occured. We already know about it and working on it.</p>',
+    },
   },
   ru: {
     404: {
-      title: "Алина Шевякова. Страница не найдена",
-      header: "404: Страница не найдена",
-      descr: '<p>К сожалению мы не можем найти страницу, которую вы ищете. Возможно она была удалена</p>'
+      title: 'Алина Шевякова. Страница не найдена',
+      header: '404: Страница не найдена',
+      descr:
+        '<p>К сожалению мы не можем найти страницу, которую вы ищете. Возможно она была удалена</p>',
     },
     500: {
-      title: "Алина Шевякова. Произошла ошибка",
-      header: "500: Что-то случилось",
-      descr: '<p>К сожалению что-то пошло не так. Мы знаем об этой проблеме и уже занимаемся ею</p><p>Попробуйте открыть другую страницу</p>'
-    }
-  }
+      title: 'Алина Шевякова. Произошла ошибка',
+      header: '500: Что-то случилось',
+      descr:
+        '<p>К сожалению что-то пошло не так. Мы знаем об этой проблеме и уже занимаемся ею</p><p>Попробуйте открыть другую страницу</p>',
+    },
+  },
 };
--- lang/general.js
+++ lang/general.js
@@ -1,22 +1,23 @@
 module.exports = {
-    en: {
-        alina: 'Alina Shevyakova',
-        painting: 'Painting',
-        graphic: 'Graphic',
-        oilOnCanvas: 'Oil on canvas',
-        title: 'Alina Shevyakova. Painting, graphic',
-        copyright: '2009-2013, Alina Shevyakova',
-        description: 'Website of Alina Shevyakova. Painting, graphic',
-        keywords: 'paintings, painting, graphic, gallery, drawings, artist, art'
-    },
-    ru: {
-        alina: "Алина Шевякова",
-        painting: "Живопись",
-        graphic: "Графика",
-        oilOnCanvas: 'холст, масло',
-        title: "Алина Шевякова. Живопись, графика",
-        copyright: '2009-2013, Алина Шевякова',
-        description: 'Сайт-галерея Алины Шевяковой. Живопись, графика',
-        keywords: 'картины, графика, живопись, рисунки, галерея, художники, искусство, творчество'
-    }
+  en: {
+    alina: 'Alina Shevyakova',
+    painting: 'Painting',
+    graphic: 'Graphic',
+    oilOnCanvas: 'Oil on canvas',
+    title: 'Alina Shevyakova. Painting, graphic',
+    copyright: '2009-2013, Alina Shevyakova',
+    description: 'Website of Alina Shevyakova. Painting, graphic',
+    keywords: 'paintings, painting, graphic, gallery, drawings, artist, art',
+  },
+  ru: {
+    alina: 'Алина Шевякова',
+    painting: 'Живопись',
+    graphic: 'Графика',
+    oilOnCanvas: 'холст, масло',
+    title: 'Алина Шевякова. Живопись, графика',
+    copyright: '2009-2013, Алина Шевякова',
+    description: 'Сайт-галерея Алины Шевяковой. Живопись, графика',
+    keywords:
+      'картины, графика, живопись, рисунки, галерея, художники, искусство, творчество',
+  },
 };
